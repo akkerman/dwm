952927de36e8fdf5392366874428027f8680fdca
Marcel Akkerman <ikbenmarcelakkerman@hotmail.com>
2019-01-29 22:30:31 +0100

    Use slock to lock screen

--- config.h
+++ config.h
@@ -65,7 +65,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *roficmd[] = { "rofi", "-show", "drun" };
-static const char *lockcmd[] = { "/home/akkerman/.config/i3/lock.sh" };
+static const char *lockcmd[] = { "slock" };
 static const char *browse[] = { "chromium", "--profile-directory=Default" };
 static const char *chscreen[] = { "/home/akkerman/.config/rofi/chscreen.sh" };
 
